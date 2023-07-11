ead0a3819e89bd53cc60d8a3c725c003a2a64695
findmytrueself <load1999@gmail.com>
2023-07-11 17:24:44 +0900

    fix : mobile small size

--- src/vue/components/RecentTransactions.vue
+++ src/vue/components/RecentTransactions.vue
@@ -401,6 +401,9 @@ export default {
             @media screen and (max-width: 480px) {
               width: 28% !important;
             }
+            @media screen and (max-width: 360px) {
+              width: 20.5% !important;
+            }
           }
 
           &:nth-child(4) {
@@ -416,6 +419,9 @@ export default {
 
             @media screen and (max-width: 480px) {
               width: 22% !important;
+            }
+            @media screen and (max-width: 360px) {
+              width: 5% !important;
             }
           }
         }
@@ -504,7 +510,10 @@ export default {
             width: 24.5% !important;
 
             @media screen and (max-width: 480px) {
-              width: 22.4% !important;
+              width: 20% !important;
+            }
+            @media screen and (max-width: 360px) {
+              width: 15% !important;
             }
           }
 
@@ -525,7 +534,7 @@ export default {
               width: 16% !important;
             }
             @media screen and (max-width: 480px) {
-              width: 23% !important;
+              width: 15% !important;
             }
           }
         }
