25738993b7cc38b801227f0049e3312da304d19e
findmytrueself <load1999@gmail.com>
2023-09-04 18:46:42 +0900

    fix: bugfix contract address

--- src/vue/pages/Transaction.vue
+++ src/vue/pages/Transaction.vue
@@ -526,20 +526,18 @@ export default {
     },
     txMeta() {
       ;(async () => {
-        const response = await (
-          await this.$fetch.get(`${cfg.API_URL}/contractTx`, {
-            q: `_id:${this.txMeta.to}`,
-          })
-        ).json()
-        if (response.hits.length > 0) {
+        const response = await this.$fetch.get(`${cfg.API_URL}/contractTx`, {
+          q: `_id:${this.txMeta.contract}`,
+        })
+        const responseJson = await response.json()
+
+        if (responseJson.hits.length > 0) {
           this.isContract = true
         }
       })()
     },
   },
-  updated() {
-    console.log(this.events, 'events')
-  },
+
   mounted() {
     if (this.$route.query.payload) {
       this.selectedPayloadTab =
@@ -593,30 +591,29 @@ export default {
       this.error = null
       let hash = this.$route.params.hash
       ;(async () => {
-        const response = await (
-          await this.$fetch.get(`${cfg.API_URL}/transactions`, {
-            q: `_id:${hash}`,
-          })
-        ).json()
-        if (response.hits.length) {
-          this.txMeta = response.hits[0].meta
+        const response = await this.$fetch.get(`${cfg.API_URL}/transactions`, {
+          q: `_id:${hash}`,
+        })
+        const responseJson = await response.json()
+
+        if (responseJson.hits.length) {
+          this.txMeta = responseJson.hits[0].meta
         }
       })()
       ;(async () => {
         const start = (this.currentPage - 1) * this.itemsPerPage
-        const response = await (
-          await this.$fetch.get(`${cfg.API_URL}/event`, {
-            q: `tx_id:${hash}`,
-            from: start,
-            size: this.itemsPerPage,
-          })
-        ).json()
-        if (response.error) {
-          this.error = response.error.msg
-        } else if (response.hits.length) {
-          this.totalEvents = response.total
-          this.events = response.hits
-          this.limitPageTotalCount = response.total
+        const response = await this.$fetch.get(`${cfg.API_URL}/event`, {
+          q: `tx_id:${hash}`,
+          from: start,
+          size: this.itemsPerPage,
+        })
+        const responseJson = await response.json()
+        if (responseJson.error) {
+          this.error = responseJson.error.msg
+        } else if (responseJson.hits.length) {
+          this.totalEvents = responseJson.total
+          this.events = responseJson.hits
+          this.limitPageTotalCount = responseJson.total
         } else {
           this.events = []
           this.limitPageTotalCount = 0
