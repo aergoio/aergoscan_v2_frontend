e8af8db3d3a7f43f5430e08cbba720b7f27df37e
findmytrueself <load1999@gmail.com>
2023-09-07 15:31:53 +0900

    fix: icon image_url

--- src/vue/components/AccountNftInventoryTable.vue
+++ src/vue/components/AccountNftInventoryTable.vue
@@ -1,5 +1,9 @@
 <template>
-  <data-table :trans-data="data || []" :is-loading="isLoading" :css="dataTableCss">
+  <data-table
+    :trans-data="data || []"
+    :is-loading="isLoading"
+    :css="dataTableCss"
+  >
     <template slot="error" v-if="error">
       <div class="error nft-inventory show">
         {{ error }}
@@ -10,12 +14,16 @@
         <div>{{ header.text }}</div>
       </th>
     </template>
-    <template slot="list" slot-scope="{row}">
+    <template slot="list" slot-scope="{ row }">
       <td>
         <div>
-          <span class="identicon default" v-if="!row.image"></span>
-          <span class="identicon" v-else><img :src="row.image"></span>
-          <router-link :to="`/nft/${row.symbolHash}/`" class="address txt-ellipsis">{{ row.name }}</router-link>
+          <span class="identicon default" v-if="!row.image_url"></span>
+          <span class="identicon" v-else><img :src="row.image_url" /></span>
+          <router-link
+            :to="`/nft/${row.symbolHash}/`"
+            class="address txt-ellipsis"
+            >{{ row.name }}</router-link
+          >
         </div>
       </td>
       <td>
@@ -23,6 +31,7 @@
       </td>
       <td>
         <div>
+          <Identicon :text="row.address" size="18" class="mini-identicon" />
           <router-link :to="`/nft/${row.symbolHash}/`" class="address">
             {{ row.address }}
           </router-link>
@@ -30,33 +39,38 @@
       </td>
       <td>
         <div>
-          <router-link :to="`/nft/${row.address}/?tx=inventory&keyword=${address}`" class="address">
+          <router-link
+            :to="`/nft/${row.address}/?tx=inventory&keyword=${address}`"
+            class="address"
+          >
             {{ row.amount }}
           </router-link>
         </div>
       </td>
       <td>
-        <div class="tooltipped tooltipped-se tooltipped-align-left-2"
-             :aria-label="moment(row.ts).format('dddd, MMMM Do YYYY, HH:mm:ss')">
+        <div
+          class="tooltipped tooltipped-se tooltipped-align-left-2"
+          :aria-label="moment(row.ts).format('dddd, MMMM Do YYYY, HH:mm:ss')"
+        >
           {{ moment(row.ts).format('YYYY-MM-DD HH:mm:ss') }}
         </div>
       </td>
     </template>
     <pagination
-        slot="pagination"
-        :css="paginationCss"
-        :page="currentPage"
-        :total-items="limitPageTotalCount"
-        :itemsPerPage="itemsPerPage"
-        @onUpdate="changePage"
-        @updateCurrentPage="updateCurrentPage"
+      slot="pagination"
+      :css="paginationCss"
+      :page="currentPage"
+      :total-items="limitPageTotalCount"
+      :itemsPerPage="itemsPerPage"
+      @onUpdate="changePage"
+      @updateCurrentPage="updateCurrentPage"
     />
   </data-table>
 </template>
 <script>
-
-import cfg from '@/src/config';
-import moment from 'moment';
+import cfg from '@/src/config'
+import moment from 'moment'
+import Identicon from './Identicon.vue'
 
 export default {
   name: 'AccountNftInventoryTable',
@@ -73,21 +87,21 @@ export default {
     },
     initialPage: {
       type: Number,
-      default: 1
+      default: 1,
     },
     itemsPerPage: {
       type: Number,
-      default: 10
+      default: 10,
     },
     defaultSort: String,
     defaultSortDirection: String,
     sortField: {
       type: String,
-      default: 'blockno'
+      default: 'blockno',
     },
     sort: {
       type: String,
-      default: 'desc'
+      default: 'desc',
     },
   },
   data() {
@@ -99,8 +113,8 @@ export default {
       isLoading: false,
       currentPage: this.initialPage,
       paginationCss: {
-        pagination: "pagination nft-inventory-table",
-        paginationInner: "pagination-inner",
+        pagination: 'pagination nft-inventory-table',
+        paginationInner: 'pagination-inner',
         moveFirstPage: 'pprev',
         movePreviousPage: 'prev',
         moveNextPage: 'next',
@@ -110,87 +124,93 @@ export default {
       sortedDir: this.sort,
     }
   },
-  created() {
-  },
-  beforeDestroy() {
-  },
+  created() {},
+  beforeDestroy() {},
   computed: {
     headers() {
       return [
-        {text: 'NAME', value: 'name'},
-        {text: 'SYMBOL', value: 'symbol'},
-        {text: 'ADDRESS', value: 'address'},
-        {text: 'TOKEN COUNT', value: 'tokencount'},
-        {text: 'LATEST TRANSACTION', value: 'latesttransaction'},
+        { text: 'NAME', value: 'name' },
+        { text: 'SYMBOL', value: 'symbol' },
+        { text: 'ADDRESS', value: 'address' },
+        { text: 'TOKEN COUNT', value: 'tokencount' },
+        { text: 'LATEST TRANSACTION', value: 'latesttransaction' },
       ]
     },
     dataTableCss() {
       return {
         wrapper: 'tab-content nft-inventory' + (this.active ? ' active' : ''),
-        table: "nft-inventory-table" + (this.isLoading ? ' is-loading' : ''),
-      };
+        table: 'nft-inventory-table' + (this.isLoading ? ' is-loading' : ''),
+      }
     },
     isHidePage() {
       return this.itemsPerPage >= this.limitPageTotalCount
-    }
+    },
   },
   mounted() {
-    this.changePage(this.currentPage);
+    this.changePage(this.currentPage)
   },
   methods: {
-    loadInventoryTableData: async function ({id, sortField, sort, currentPage, itemsPerPage}) {
-      this.error = "";
-      const start = (currentPage - 1) * itemsPerPage;
-      const response = await (await this.$fetch.get(`${cfg.API_URL}/nftGroupCountInventory`, {
-        q: `account:${id}`,
-        size: itemsPerPage,
-        from: start,
-        sort: `${sortField}:${sort}`,
-      })).json();
+    loadInventoryTableData: async function ({
+      id,
+      sortField,
+      sort,
+      currentPage,
+      itemsPerPage,
+    }) {
+      this.error = ''
+      const start = (currentPage - 1) * itemsPerPage
+      const response = await (
+        await this.$fetch.get(`${cfg.API_URL}/nftGroupCountInventory`, {
+          q: `account:${id}`,
+          size: itemsPerPage,
+          from: start,
+          sort: `${sortField}:${sort}`,
+        })
+      ).json()
       if (response.error) {
-        this.error = response.error.msg;
+        this.error = response.error.msg
       } else if (response.hits.length) {
         this.data = response.hits.map((item, index) => ({
-          ts:item.ts,
+          ts: item.ts,
           address: item.address,
           amount: item.amount,
-          image: item.token.meta.image,
+          image_url: item.token.meta.image_url,
           name: item.token.meta.name,
           symbol: item.token.meta.symbol,
           symbolHash: item.token.hash,
           decimals: item.token.meta.decimals,
-        }));
-        this.totalItems = response.total;
-        this.limitPageTotalCount = response.limitPageCount;
+        }))
+        this.totalItems = response.total
+        this.limitPageTotalCount = response.limitPageCount
       } else {
-        this.data = [];
-        this.totalItems = 0;
-        this.limitPageTotalCount = 0;
+        this.data = []
+        this.totalItems = 0
+        this.limitPageTotalCount = 0
       }
-      this.$emit('onUpdateTotalCount', this.totalItems);
+      this.$emit('onUpdateTotalCount', this.totalItems)
     },
     reload: async function (address) {
-      this.isLoading = true;
+      this.isLoading = true
       await this.loadInventoryTableData({
         id: address ? address : this.address,
         sortField: this.sortedField,
         sort: this.sortedDir,
         currentPage: this.currentPage,
         itemsPerPage: this.itemsPerPage,
-      });
-      this.isLoading = false;
+      })
+      this.isLoading = false
     },
     changePage: function (currentPage) {
-      this.currentPage = currentPage;
-      this.reload();
+      this.currentPage = currentPage
+      this.reload()
     },
     updateCurrentPage: function (currentPage) {
-      this.currentPage = currentPage;
+      this.currentPage = currentPage
     },
-    moment
+    moment,
   },
-  components: {}
-};
+  components: { Identicon },
+}
 </script>
 
 <style lang="scss" scoped>
