60509251892ef5e2fbaf599064ea8823f0e68418
findmytrueself <load1999@gmail.com>
2023-09-07 18:14:52 +0900

    feat: add interactive arrow

--- src/vue/components/QueryFunction.vue
+++ src/vue/components/QueryFunction.vue
@@ -2,8 +2,23 @@
   <div class="function-block">
     <span :class="isClick ? `function show` : `function`" @click="handleClick"
       >{{ `${number + 1}. ${name}` }}
+
+      <svg
+        fill="#ffffff"
+        width="800px"
+        height="800px"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        :class="isClick ? `arrow downbutton` : `arrow upbutton`"
+      >
+        <rect x="0" fill="none" width="24" height="24" />
+        <g>
+          <path
+            d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z"
+          />
+        </g>
+      </svg>
     </span>
-    <span :class="isClick ? `downbutton` : `upbutton`"></span>
 
     <div
       :class="isClick ? `function_body show` : `function_body hide`"
@@ -16,12 +31,28 @@
           :style="{ minHeight: '60px', marginBottom: '5px' }"
         >
           <span class="key">{{ arg.name }}</span>
-          <input
-            type="text"
-            v-model="args[arg.name]"
-            class="arg-field"
-            :style="{ marginTop: '5px' }"
-          />
+          <div
+            :style="{
+              display: 'flex',
+              width: '100%',
+              height: '35px',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }"
+          >
+            <input
+              type="text"
+              class="arg-field"
+              :style="{ marginTop: '5px', width: '90%' }"
+              v-model="args[arg.name]"
+            />
+            <img
+              class="validate"
+              src="~@assets/img/ic_fail@2x.png"
+              alt="validate"
+            />
+            <!-- <img class="validate" src="~@assets/img/ic-success@2x.png" alt="validate" /> -->
+          </div>
           <!-- <span v-if="idx != func.arguments.length - 1"></span> -->
         </div>
       </div>
@@ -39,7 +70,12 @@
       <div v-if="typeof result !== 'undefined'" class="code-highlight-pre">
         <div :style="{ display: 'flex', flexDirection: 'column' }">
           <span class="result_title">Result</span>
-          <span class="result_content" v-html="syntaxHighlight(result)"></span>
+          <router-link :to="`/transaction/${result}/`" class="result_content">
+            <span v-if="typeof result === 'string'" class="resultHash">
+              {{ result }}
+            </span>
+            <span v-else v-html="syntaxHighlight(result)"></span>
+          </router-link>
         </div>
       </div>
     </div>
@@ -49,9 +85,13 @@
 <script>
 import { loadAndWait } from '@/src/vue/utils/async'
 import { syntaxHighlight } from '@/src/vue/utils/syntax-highlight'
+import ArrowSvg from '@assets/img/arrow-right-svgrepo-com.svg'
 
 export default {
   props: ['abi', 'name', 'number', 'address', 'callContractHash', 'clickAll'],
+  components: {
+    ArrowSvg,
+  },
   data() {
     return {
       args: {},
@@ -71,8 +111,29 @@ export default {
     },
   },
   updated() {
-    console.log(this.abi, 'abi')
-    console.log(this.func, 'func')
+    console.log(this.args, 'args')
+    // try {
+    //   const parsedJSON = JSON.parse(value)
+    //   console.log(parsedJSON, 'parsedJSON')
+    //   // JSON 데이터 처리
+    //   this.args[key] = parsedJSON
+    // } catch (error) {
+    //   // JSON 파싱 실패 시 다른 유형 확인
+    //   if (value === 'true' || value === 'false') {
+    //     // 불리언 처리
+    //     const booleanValue = value === 'true'
+    //     console.log(booleanValue, 'booleanValue')
+    //     this.args[key] = booleanValue
+    //   } else if (!isNaN(value)) {
+    //     // 숫자 처리
+    //     const numberValue = parseFloat(value)
+    //     console.log(numberValue, 'numberValue')
+    //     this.args[key] = numberValue
+    //   } else {
+    //     // 문자열 처리
+    //     this.args[key] = value
+    //   }
+    // }
   },
   computed: {
     func() {
@@ -87,7 +148,6 @@ export default {
     syntaxHighlight,
     async queryContract() {
       const wait = loadAndWait()
-
       const args = this.args
       const argValues = this.func.arguments.map((arg) => args[arg.name])
       if (argValues.some((item) => typeof item === 'undefined')) {
@@ -129,6 +189,8 @@ export default {
       if (this.getActiveAccount.address) {
         const args = this.args
         const argValues = this.func.arguments.map((arg) => args[arg.name])
+        console.log(args, 'args')
+        console.log(argValues, 'argValues')
         if (argValues.some((item) => typeof item === 'undefined')) {
           this.result = {
             error: 'You did not provide all arguments',
@@ -159,7 +221,9 @@ export default {
       }
     },
     aergoConnectCall(action, responseType, d) {
+      console.log(d, 'd')
       const data = { ...d }
+      console.log(data, 'data')
       if (!d.payload && !d.payload_json) {
         data.payload = ''
       }
@@ -190,16 +254,32 @@ export default {
 </script>
 
 <style lang="scss">
-.downbutton {
-  background: url('~@assets/img/arrow-right-svgrepo-com.svg');
-  transform: rotate(90deg);
+.validate {
+  height: 35px;
+  margin-right: 14px;
 }
 
+.function {
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+  .arrow {
+    width: 20px;
+    height: 20px;
+  }
+  &:hover {
+    svg {
+      fill: #0784c3;
+    }
+  }
+}
+
+.downbutton {
+  transform: rotate(90deg);
+  transition: transform 0.3s ease-in-out;
+}
 .upbutton {
-  display: inline-block;
-  vertical-align: middle;
-  background: url('~@assets/img/arrow-right-svgrepo-com.svg');
-  background-size: auto 100%;
-  background-repeat: no-repeat;
+  transform: rotate(0deg);
+  transition: transform 0.3s ease-in-out;
 }
 </style>
