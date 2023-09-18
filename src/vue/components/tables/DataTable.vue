2c16f81979d7e574b48e08a860d7ddb19e30c371
findmytrueself <load1999@gmail.com>
2023-09-18 10:27:26 +0900

    fix: remove table category scroll

--- src/vue/components/tables/DataTable.vue
+++ src/vue/components/tables/DataTable.vue
@@ -1,25 +1,26 @@
 <template>
   <div :class="[css.wrapper]">
-    <slot name="search"/>
-    <slot name="title"/>
-    <slot name="desc"/>
-    <slot name="error"/>
-    <div class="h-scroll">
-      <table :class="[css.table, statusTableCss]">
-        <thead>
+    <slot name="search" />
+    <slot name="title" />
+    <slot name="desc" />
+    <slot name="error" />
+    <!-- <div class="h-scroll"> -->
+    <table :class="[css.table, statusTableCss]">
+      <thead>
         <tr>
-          <slot name="header"/>
+          <slot name="header" />
         </tr>
-        </thead>
-        <tbody>
-<!--        <template v-if="isLoading">-->
-<!--          <tr class="hidden loading">-->
-<!--            <td colspan="100%">loading...</td>-->
-<!--          </tr>-->
-<!--        </template>-->
+      </thead>
+      <tbody>
+        <!--        <template v-if="isLoading">-->
+        <!--          <tr class="hidden loading">-->
+        <!--            <td colspan="100%">loading...</td>-->
+        <!--          </tr>-->
+        <!--        </template>-->
         <template v-if="transData.length">
           <tr v-for="(row, index) in transData">
-            <slot name="list" :row="row"/> <!-- list slot -->
+            <slot name="list" :row="row" />
+            <!-- list slot -->
           </tr>
         </template>
         <template v-else-if="!isLoading && transData.length === 0">
@@ -27,19 +28,18 @@
             <td colspan="100%">No items found</td>
           </tr>
         </template>
-        </tbody>
-      </table>
-    </div>
+      </tbody>
+    </table>
+    <!-- </div> -->
     <template>
-      <slot name="pagination"/>
+      <slot name="pagination" />
     </template>
     <template>
-      <slot name="more"/>
+      <slot name="more" />
     </template>
   </div>
 </template>
 <script>
-
 export default {
   name: 'DataTable',
   props: {
@@ -48,30 +48,25 @@ export default {
     css: {
       type: Object,
       default: () => ({
-        wrapper:'table-wrap',
-        table: ''
-      })
+        wrapper: 'table-wrap',
+        table: '',
+      }),
     },
   },
 
   data: function () {
-    return {
-    }
+    return {}
   },
-  watch: {
-  },
-  methods: {
-  },
+  watch: {},
+  methods: {},
   computed: {
     statusTableCss() {
-      if(!this.isLoading && this.transData.length === 0) {
+      if (!this.isLoading && this.transData.length === 0) {
         return 'not-found'
       }
 
-      return this.isLoading ? 'loading' : '';
-    }
-  }
-};
+      return this.isLoading ? 'loading' : ''
+    },
+  },
+}
 </script>
-
-
