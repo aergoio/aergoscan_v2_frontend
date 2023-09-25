5c36c6b6e4d76a99470be23cc8b70afaece7ef31
Instant Coffee <73890790+findmytrueself@users.noreply.github.com>
2023-09-25 14:39:22 +0900

    chore: delete console.log (#57)
    * fix: merge conflict
    
    * chore: delete console.log

--- src/vue/components/QueryFunction.vue
+++ src/vue/components/QueryFunction.vue
@@ -189,12 +189,6 @@ export default {
     },
   },
 
-  updated() {
-    console.log(this.isLoading, 'isLoading')
-    console.log(this.result, 'result')
-    console.log(this.receipt, 'receipt')
-  },
-
   methods: {
     syntaxHighlight,
     async queryContract() {
@@ -270,6 +264,8 @@ export default {
           'AERGO_SEND_TX_RESULT',
           data
         )
+        this.isLoading = true
+        this.receipt = {}
         await wait()
         this.result = result.hash
         this.$emit('onUpdateResultHash', result.hash)
