66b9dc61302276911b8b2059854502a0ea9cb25e
findmytrueself <load1999@gmail.com>
2025-05-07 18:54:03 +0900

    Update multiple variadic arguments

--- src/vue/components/QueryFunction.vue
+++ src/vue/components/QueryFunction.vue
@@ -2,9 +2,7 @@
   <div class="function-block">
     <span :class="isClick ? `function show` : `function`" @click="handleClick">
       <div>
-        <span>
-          {{ `${number + 1}. ${name}` }}
-        </span>
+        <span>{{ `${number + 1}. ${name}` }}</span>
         <span class="contract_typemark">{{
           func.view ? '[read]' : '[write]'
         }}</span>
@@ -34,49 +32,102 @@
         <div
           v-for="arg in func.arguments"
           :key="arg.name"
-          :style="{ minHeight: '60px', marginBottom: '5px' }"
-          @click="() => addDynamicArg(arg.name)"
+          style="margin-bottom: 10px"
         >
           <span class="key">{{
             arg.name === '...' ? 'Variadic argument' : arg.name
           }}</span>
+
+          <!-- Variadic argument input -->
           <div
-            :style="{
-              display: 'flex',
-              width: '100%',
-              justifyContent: 'space-between',
-            }"
+            v-if="arg.name === '...'"
+            style="
+              display: flex;
+              flex-direction: column;
+              row-gap: 6px;
+              margin-top: 5px;
+            "
           >
-            <input
-              type="text"
-              class="arg-field"
-              :style="{ marginTop: '5px', width: '90%' }"
-              v-model="args[arg.name]"
-            />
-            <span
-              v-if="args[arg.name]"
-              :style="{ display: 'flex', alignItems: 'center' }"
+            <div
+              v-for="(val, idx) in args['...']"
+              :key="idx"
+              style="display: flex; align-items: center"
             >
+              <span
+                style="
+                  width: 20px;
+                  text-align: right;
+                  margin-right: 6px;
+                  cursor: default;
+                "
+                >{{ idx }}.</span
+              >
+              <input
+                type="text"
+                class="arg-field"
+                style="width: 100%"
+                v-model="args['...'][idx]"
+              />
+              <button
+                class="arg-btn"
+                @click="removeVariadicArg(idx)"
+                v-if="args['...'].length > 1"
+              >
+                -
+              </button>
+              <button
+                class="arg-btn"
+                @click="addVariadicArg"
+                v-if="idx === args['...'].length - 1"
+              >
+                +
+              </button>
               <img
-                v-if="jsonValidate(args[arg.name])"
+                v-if="jsonValidate(args['...'][idx])"
                 class="validate"
                 src="~@assets/img/ic-success@2x.png"
               />
               <img v-else class="validate" src="~@assets/img/ic_fail@2x.png" />
+            </div>
+          </div>
+
+          <!-- Regular argument input -->
+          <div
+            v-else
+            style="display: flex; align-items: center; margin-top: 5px"
+          >
+            <input
+              type="text"
+              class="arg-field"
+              style="width: 100%"
+              v-model="args[arg.name]"
+            />
+            <span
+              v-if="args[arg.name]"
+              style="display: flex; align-items: center"
+            >
+              <img
+                v-if="jsonValidate(args[arg.name])"
+                class="validate"
+                src="~@assets/img/ic-success@3x.png"
+              />
+              <img v-else class="validate" src="~@assets/img/ic_fail@3x.png" />
               <span
                 v-if="jsonValidate(args[arg.name])"
-                :style="{
-                  whiteSpace: 'nowrap',
-                  marginRight: '10px',
-                  textTransform: 'capitalize',
-                }"
-                >{{ jsonTypeof(args[arg.name]) }}</span
+                style="
+                  white-space: nowrap;
+                  margin-left: 10px;
+                  text-transform: capitalize;
+                "
               >
+                {{ jsonTypeof(args[arg.name]) }}
+              </span>
             </span>
           </div>
         </div>
       </div>
-      <span class="btn-call" v-on:click="queryContract"> Query </span>
+
+      <span class="btn-call" @click="queryContract">Query</span>
       <span
         class="btn-call"
         v-if="!func.view"
@@ -86,12 +137,14 @@
       >
         Call
       </span>
+
       <div v-if="isLoading" class="loadingProgress" />
+
       <div
         v-if="typeof result !== 'undefined' && !isLoading"
         class="code-highlight-pre"
       >
-        <div :style="{ display: 'flex', flexDirection: 'column' }">
+        <div style="display: flex; flex-direction: column">
           <div class="result_wrapper">
             <span class="result_title">Result</span>
             <router-link
@@ -101,7 +154,6 @@
             >
               <span v-html="result" class="result_content" />
             </router-link>
-
             <span
               v-else-if="typeof result === 'string' && !receipt"
               v-html="result"
@@ -118,14 +170,11 @@
             <span class="result_title">
               <span>Status:</span>
               <img
-                class="status_img"
                 v-if="receipt?.status === 'ERROR'"
-                src="~@assets/img/ic_fail@1x.png"
+                src="~@assets/img/ic_fail.png"
               />
               <img v-else src="~@assets/img/ic-success.png" />
-              <span class="receipt_text">
-                {{ receipt?.status }}
-              </span>
+              <span class="receipt_text">{{ receipt?.status }}</span>
             </span>
             <span v-html="receipt?.result" class="result_content" />
             <span v-if="!receipt?.result" class="result_content empty"
@@ -141,16 +190,12 @@
 <script>
 import { loadAndWait } from '@/src/vue/utils/async'
 import { syntaxHighlight } from '@/src/vue/utils/syntax-highlight'
-import ArrowSvg from '@assets/img/arrow-right-svgrepo-com.svg'
 
 export default {
   props: ['abi', 'name', 'number', 'address', 'callContractHash', 'clickAll'],
-  components: {
-    ArrowSvg,
-  },
   data() {
     return {
-      args: { '...': null },
+      args: { '...': ['""'] },
       result: void 0,
       isLoading: false,
       isClick: false,
@@ -159,23 +204,30 @@ export default {
     }
   },
 
+  mounted() {
+    this.func.arguments.forEach((arg) => {
+      if (!this.args[arg.name]) {
+        if (arg.name === '...') {
+          this.$set(this.args, arg.name, '""')
+        } else {
+          this.$set(this.args, arg.name, '""')
+        }
+      }
+    })
+  },
+
   watch: {
     clickAll() {
-      if (!this.clickAll) {
-        this.isClick = true
-      } else {
-        this.isClick = false
-      }
+      this.isClick = !this.clickAll
     },
     async result() {
-      const hash = this.result
       const wait = loadAndWait()
       this.isLoading = true
       await wait()
-      const isBase58Hash = this.isBase58Hash(hash)
+      const isBase58Hash = this.isBase58Hash(this.result)
       this.receipt = isBase58Hash
         ? await this.$store.dispatch('blockchain/fetchTransactionReceipt', {
-            hash,
+            hash: this.result,
           })
         : null
       this.isLoading = false
@@ -187,72 +239,73 @@ export default {
       return this.abi.functions.find((func) => func.name === this.name) || {}
     },
     getActiveAccount() {
-      return this.$store.getters[`blockchain/getActiveAccount`]
+      return this.$store.getters['blockchain/getActiveAccount']
     },
   },
 
   methods: {
     syntaxHighlight,
+
+    addVariadicArg() {
+      this.args['...'].push('""')
+    },
+    removeVariadicArg(idx) {
+      if (this.args['...'].length > 1) {
+        this.args['...'].splice(idx, 1)
+      }
+    },
+
     async queryContract() {
       const wait = loadAndWait()
-      const args = this.args
       const argValues = this.func.arguments.map((arg) =>
-        JSON.parse(args[arg.name])
+        arg.name === '...'
+          ? this.args['...'].map((v) => JSON.parse(v))
+          : JSON.parse(this.args[arg.name])
       )
+
       if (argValues.some((item) => typeof item === 'undefined')) {
-        this.result = {
-          error: 'You did not provide all arguments',
-        }
+        this.result = { error: 'You did not provide all arguments' }
         return
       }
 
       this.isLoading = true
       this.result = void 0
 
-      let result
       try {
-        result = await this.$store.dispatch('blockchain/queryContract', {
+        this.result = await this.$store.dispatch('blockchain/queryContract', {
           name: this.name,
           args: argValues,
           abi: this.abi,
           address: this.address,
         })
       } catch (e) {
-        let errorMsg
-        try {
-          errorMsg = e.metadata.headersMap['grpc-message']
-        } catch (_) {
-          errorMsg = e
-        }
-        result = { error: errorMsg }
+        this.result = { error: e?.metadata?.headersMap?.['grpc-message'] || e }
       }
+
       await wait()
-      this.result = result
       this.isLoading = false
     },
-    handleClick() {
-      if (this.func.arguments.length === 0 && this.func.view && !this.result) {
-        this.queryContract()
-      }
-      this.isClick = !this.isClick
-    },
+
     async callToConnect() {
       const wait = loadAndWait()
       if (this.getActiveAccount.address) {
-        const args = this.args
         const argValues = this.func.arguments.map((arg) =>
-          JSON.parse(args[arg.name])
-        )
+          arg.name === '...'
+            ? this.args['...'].map((v) => JSON.parse(v))
+            : JSON.parse(this.args[arg.name])
+        )[0]
+        console.log(argValues, 'argValues')
+
         if (argValues.some((item) => typeof item === 'undefined')) {
-          this.result = {
-            error: 'You did not provide all arguments',
-          }
+          this.result = { error: 'You did not provide all arguments' }
           return
         }
+
         const payload_json = {
           Name: this.name,
           Args: [...argValues],
         }
+
         const data = {
           amount: '0 aergo',
           from: this.getActiveAccount.address,
@@ -261,6 +314,7 @@ export default {
           to: this.address,
           type: 5,
         }
+
         const result = await this.aergoConnectCall(
           'SEND_TX',
           'AERGO_SEND_TX_RESULT',
@@ -281,9 +335,10 @@ export default {
         await this.callToConnect()
       }
     },
+
     aergoConnectCall(action, responseType, d) {
       const data = { ...d }
-      if (!d.payload && !d.payload_json) {
+      if (!data.payload && !data.payload_json) {
         data.payload = ''
       }
       return new Promise((resolve, reject) => {
@@ -298,45 +353,40 @@ export default {
           },
           { once: true }
         )
-        window.postMessage(
-          {
-            type: 'AERGO_REQUEST',
-            action: action,
-            data: data,
-          },
-          '*'
-        )
+        window.postMessage({ type: 'AERGO_REQUEST', action, data }, '*')
       })
     },
+
     jsonValidate(value) {
       try {
         JSON.parse(value)
         return true
-      } catch (e) {
+      } catch {
         return false
       }
     },
-    addDynamicArg(name) {
-      if (!this.args[name]) {
-        this.$set(this.args, name, '""')
-      }
-    },
+
     jsonTypeof(value) {
-      const parsedValue = JSON.parse(value)
-      if (typeof parsedValue === 'object') {
-        if (Array.isArray(parsedValue)) {
-          return 'Array'
-        } else {
-          return 'JSON'
-        }
-      } else {
-        return typeof parsedValue
-      }
+      const parsed = JSON.parse(value)
+      return Array.isArray(parsed) ? 'Array' : typeof parsed
     },
+
     isBase58Hash(hash) {
-      const base58Regex =
-        /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{44,64}$/
-      return base58Regex.test(hash)
+      return /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{44,64}$/.test(
+        hash
+      )
+    },
+
+    handleClick() {
+      if (
+        !this.isClick &&
+        this.func.arguments.length === 0 &&
+        this.func.view &&
+        !this.result
+      ) {
+        this.queryContract()
+      }
+      this.isClick = !this.isClick
     },
   },
 }
@@ -344,9 +394,9 @@ export default {
 
 <style lang="scss">
 .validate {
-  height: 35px;
-  margin-left: 8px;
-  margin-right: 8px;
+  height: 24px;
+  margin-left: 6px;
+  margin-right: 6px;
   position: relative;
   top: 2px;
 }
